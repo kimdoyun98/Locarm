28aa759dd3198b6db74580f367d7faa5ed128a27
kimdoyun98 <rladlsxo1250@gmail.com>
2025-06-02 20:52:05 +0900

    [Refactor] Application에서 채널 생성 삭제
    미사용

--- app/src/main/java/com/project/locarm/common/MyApplication.kt
+++ app/src/main/java/com/project/locarm/common/MyApplication.kt
@@ -1,10 +1,6 @@
 package com.project.locarm.common
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
 import com.naver.maps.map.NaverMapSdk
 import com.project.locarm.BuildConfig
 
@@ -13,15 +9,10 @@ class MyApplication : Application() {
         lateinit var prefs: PreferenceUtil
         lateinit var instance: MyApplication
         lateinit var serviceLocator: ServiceLocator
-
-        private val CHANNEL_ID = "CHANNEL_ID"
-        private val CHANNEL_NAME = "CHANNEL_NAME"
-        private val CHANNEL_DESCRITION = "CHANNEL_DESCRITION"
     }
 
     override fun onCreate() {
         prefs = PreferenceUtil(applicationContext)
-        createChannel()
         super.onCreate()
 
         instance = this
@@ -29,19 +20,5 @@ class MyApplication : Application() {
 
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient(BuildConfig.Client_ID)
-    }
-
-    private fun createChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance).apply {
-                description = CHANNEL_DESCRITION
-
-            }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
     }
 }
